3940a4373823aa76514f7fa380a38d462643d249
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-09-14 23:19:15 +0200

    removing spec dirs from modules

--- resolve.sh
+++ resolve.sh
@@ -11,6 +11,8 @@ r10k puppetfile install --verbose info --moduledir "${puppet_modules}" --puppetf
 
 # remove .git directories
 find "${puppet_modules}" -name ".git" -prune -exec rm -rf {} \;
+# remove spec directories
+find "${puppet_modules}" -maxdepth 2 -mindepth 2 -name "spec" -type d -prune -exec rm -rf {} \;
 
 chown --reference="${puppet_file}" -R "${puppet_modules}"/
 chown --reference="${puppet_file}" -R "${cache_dir}"/
